6f22e6f6fa23c1bb7162d371e2aeeed9fad02d8a
valery-chumak <blossssom88@gmail.com>
2023-11-30 18:40:18 +0200

    Navbar element

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import { ArrowDropDown, Notifications, Search } from "@mui/icons-material";
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="container-left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt="netflix logo"
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="container-right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications className="icon" />
+          <img
+            src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+            alt="avatar"
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../components/Navbar/Navbar";
+import "./Homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="home">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Homepage;
