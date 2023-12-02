e0eee21f1c3368383cf9802e561dbed957d7491c
valery-chumak <blossssom88@gmail.com>
2023-12-02 15:58:37 +0200

    login page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
 import Homepage from "./pages/Homepage/Homepage";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import MoviePage from "./pages/MoviePage/MoviePage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 function App() {
-  return <RegisterPage />;
+  return <LoginPage />;
 }
 
 export default App;
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,38 @@
 import "./LoginPage.scss";
 
 const LoginPage = () => {
-  return <div className="login">LoginPage</div>;
+  return (
+    <div className="login">
+      <div className="top">
+        <div className="wrapper">
+          <img
+            className="logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="container">
+        <form>
+          <h1>Sign In</h1>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email or Phone number"
+          />
+          <input type="password" name="password" placeholder="Password" />
+          <button className="login-button">Sign In</button>
+          <span className="text">
+            New to Netflix? <b>Sign up now</b>
+          </span>
+          <span className="copyright">
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. <b>Learn more</b>.
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
