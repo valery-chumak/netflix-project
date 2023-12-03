3b5c1d63208f4ef4c43059aba040b9cb470a35dd
valery-chumak <blossssom88@gmail.com>
2023-12-04 00:44:44 +0200

    movies routes

---
+++ api/routes/movies.js
@@ -0,0 +1,82 @@
+const router = require("express").Router();
+const verify = require("../middlewares/verifyToken");
+const Movie = require("../models/Movie");
+
+//create
+router.post("/", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body);
+    try {
+      const movie = await newMovie.save();
+      res.status(201).json(movie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+//get random
+router.get("/random", verify, async (req, res) => {
+  const type = req.query.type;
+  let movie;
+  try {
+    if (type === series) {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: true } },
+        { $sample: { size: 1 } },
+      ]);
+    } else {
+      movie = await Movie.aggregate([
+        { $match: { isSeries: false } },
+        { $sample: { size: 1 } },
+      ]);
+    }
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+//update
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedMovie);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+//delete
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await Movie.findByIdAndDelete(req.params.id);
+      res.status(200).json("The movie was deleted");
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("You are not allowed");
+  }
+});
+//get
+router.get("/:id", verify, async (req, res) => {
+  try {
+    const movie = await Movie.findById(req.params.id);
+    res.status(200).json(movie);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
